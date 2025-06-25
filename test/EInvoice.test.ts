f4238675e287fb885b8f5bdd9968334f00f369d3
Jan Vaorin <70795482+corgijan@users.noreply.github.com>
2025-06-25 08:37:33 +0200

    Update EInvoice.test.ts

--- test/EInvoice.test.ts
+++ test/EInvoice.test.ts
@@ -61,5 +61,6 @@ test('simpleEInvoiceTest', () => {
   }
   let result_xml = generateEInvoiceXML(einvoice)
   // if you read that validate at https://www.epoconsulting.com/einvoice-sap/e-rechnung-viewer
+  // for reference https://www.tuv.com/content-media-files/germany/corporate_communication/e-invoice/beispiel-xrechnung-cii.xml
   console.log(result_xml)
 })
